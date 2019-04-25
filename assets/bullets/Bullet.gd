bb0c885562f578df26b963f8124ba9b2455599e3
Vishnu Raveendran <vishnunilambur@gmail.com>
2019-04-25 22:42:43 +0530

    changed ship movement

--- assets/bullets/Bullet.gd
+++ assets/bullets/Bullet.gd
@@ -1,17 +1,21 @@
 extends Area2D
 
 const VELOCITY = -500
+var velcoity = Vector2(0, -500)
 
 func _process(delta):
 	move(delta)
 	removeWhenOffScreen()
 	var overlapping_areas = get_overlapping_areas()
+	
 	if len(overlapping_areas) > 0:
 		if overlapping_areas[0].name == 'EnemyShip':
 			self.queue_free()
 
 func move(delta):
-	global_position.y += VELOCITY*delta
+	var vel = velcoity.rotated(global_rotation)*delta
+	global_position.x += vel.x
+	global_position.y += vel.y
 	
 func removeWhenOffScreen():
 	if global_position.y < 0:
--- assets/ships/PlayerShip.gd
+++ assets/ships/PlayerShip.gd
@@ -1,36 +1,36 @@
 extends KinematicBody2D
 
-var motion = Vector2()
 var MOVE_SPEED = 200
+var velocity = Vector2(0, -200)
 const BULLET = preload("res://assets/bullets/Bullet.tscn")
 const RELOAD_TIME = 0.4
 var reloading = 0.0
 
 func get_input():
-	if Input.is_action_pressed("ui_left"):
-		motion.x = -MOVE_SPEED
-	elif Input.is_action_pressed("ui_right"):
-		motion.x = MOVE_SPEED
+	var rotate = 0
+	if Input.is_action_pressed("ui_right"):
+		rotate = deg2rad(5)
+	elif Input.is_action_pressed("ui_left"):
+		rotate = deg2rad(-5)
 	else:
-		motion.x = 0
-		
+		rotate = 0
 	if Input.is_action_pressed("ui_up"):
 		fire()
 	
-	return motion
-	
+	self.rotate(rotate)
+	velocity = velocity.rotated(rotate)
+	return velocity
 
 func fire():
 	if reloading <= 0.0:
 		var bullet = BULLET.instance()
 		bullet.global_position = global_position
+		bullet.global_rotation = global_rotation
 		get_parent().add_child(bullet)
 		reloading = RELOAD_TIME
 
-
 func _physics_process(delta):
-	motion = move_and_slide(get_input())
-
+	velocity = move_and_slide(get_input())
 
 func _process(delta):
 	reloading -= delta
